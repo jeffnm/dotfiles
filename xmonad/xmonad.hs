84d91bdcef53bfac65eaf5a0c3d062849263d940
jeffnm <jeffmudge@gmail.com>
2021-02-23 19:51:02 -0600

    Update xmonad with more hooks, scratchpad, and change in compositor

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,31 +1,27 @@
 import XMonad
-
-import XMonad.Util.EZConfig
-import XMonad.Util.Run
-import XMonad.Util.SpawnOnce
-
-import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.ManageHelpers
-import XMonad.Hooks.InsertPosition
-import XMonad.Hooks.SetWMName
 import XMonad.Actions.SpawnOn
 import XMonad.Actions.WorkspaceNames
-
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.InsertPosition
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.SetWMName
 import XMonad.Layout.Fullscreen
 import XMonad.Layout.Gaps
-import XMonad.Layout.Spacing
-import XMonad.Layout.Tabbed
-import XMonad.Layout.ToggleLayouts
+import XMonad.Layout.LayoutCombinators as LC
 import XMonad.Layout.MultiToggle
 import XMonad.Layout.MultiToggle.Instances
 import XMonad.Layout.NoBorders
-import XMonad.Layout.LayoutCombinators as LC
-
+import XMonad.Layout.Spacing
+import XMonad.Layout.Tabbed
+import XMonad.Layout.ToggleLayouts
 import XMonad.Operations
-
 import qualified XMonad.StackSet as W
+import XMonad.Util.EZConfig
+import XMonad.Util.NamedScratchpad
+import XMonad.Util.Run
+import XMonad.Util.SpawnOnce
 
 main = do
   xmproc <- spawnPipe ("xmobar ~/.config/xmobar/right -d") 
@@ -36,13 +32,12 @@ main = do
     , borderWidth   = myBorderWidth
     , layoutHook    = myLayout
     , logHook       = myLogHook xmproc
-    , manageHook    = insertPosition Master Newer <+> myManageHook <+> manageDocks <+> manageHook defaultConfig
+    , manageHook    = insertPosition Master Newer <+> myManageHook <+> manageDocks <+> manageHook defaultConfig <+> namedScratchpadManageHook myScratchPads <+> myGameHook
     , startupHook   = myStartupHook
     , workspaces    = myWorkspaces
     , handleEventHook = docksEventHook
     }
     `additionalKeys` myKeys
-
 
 -- Major Variables
 myTerminal    = "xfce4-terminal"
@@ -57,18 +52,18 @@ xmobarCurrentWorkspaceColor = "#CEFFAC"
 -- My Key Bindings
 myKeys = [
   -- APPLICATION SHORTCUTS
-  -- Open Terminal
-    ((myModMask, xK_Return), (spawnToWorkspace myTerminal wsp1 ))
+  -- Open Terminal (either in dedicated workspace or in scratchpad)
+    ((myModMask .|. shiftMask, xK_Return), (spawnToWorkspace myTerminal wsp1 ))
+  , ((myModMask, xK_Return), (namedScratchpadAction myScratchPads "terminal"))
   -- Open Browser
   , ((myModMask, xK_b), (spawn myBrowser))
-  
+
   -- WINDOW/LAYOUT MANAGEMENT
   -- Close Focused Window
   , ((myModMask, xK_c), (kill))
   -- Fullscreen focused window
-  , ((myModMask, xK_f), (chooseLayout "Full")) 
-  , ((myModMask, xK_t), (chooseLayout "Tabbed Simplest"))  
-  , ((myModMask, xK_l), (chooseLayout "Spacing Tall"))  
+  , ((myModMask, xK_f), (chooseLayout "Full"))
+  ,  ((myModMask .|. shiftMask, xK_f), (chooseLayout "Spacing Tall"))
   -- Rofi Apps
   -- Open Rofi launcher
   , ((myModMask, xK_i), (spawn "exe=`rofi -modi drun -show drun -line-padding 4 -columns 2 -padding 50 -hide-scrollbar`"))
@@ -89,121 +84,165 @@ myKeys = [
 
   --
   -- Keys for Gaps/Spacing
-  , ((myModMask .|. controlMask, xK_g), (toggleWindowSpacingEnabled)) -- toggle window spacing
-  , ((myModMask .|. shiftMask, xK_g), (sequence_ [toggleScreenSpacingEnabled, toggleWindowSpacingEnabled])) -- toggle screen and window spacing
-  
-  -- Navigation Keys
-  , ((myModMask, xK_Right), ( windows W.focusDown))
-  , ((myModMask, xK_Left), ( windows W.focusUp))
+  ,  ((myModMask .|. controlMask, xK_g), (toggleWindowSpacingEnabled)) -- toggle window spacing
+  ,  ((myModMask .|. shiftMask, xK_g), (sequence_ [toggleScreenSpacingEnabled, toggleWindowSpacingEnabled])) -- toggle screen and window spacing
 
-  ] ++ [
-      ((myModMask, key), (windows $ W.greedyView ws))
-        | (key,ws) <- myExtraWorkspaces
-      ] ++ [
-        ((myModMask .|. shiftMask, key), (windows $ W.shift ws))
-        | (key,ws) <- myExtraWorkspaces
-      ]
+  -- Toggle compositor (turn off compositing for game performance)
+  , ((myModMask, xK_v), (spawn "exe=`~/.xmonad/compositor-toggle.sh`"))
+  -- Navigation Keys
+  , ((myModMask, xK_Right), (windows W.focusDown))
+  , ((myModMask, xK_Left), (windows W.focusUp))
+  ] 
+  ++ [ ((myModMask, key), (windows $ W.greedyView ws))
+         | (key, ws) <- myExtraWorkspaces
+       ]
+    ++ [ ((myModMask .|. shiftMask, key), (windows $ W.shift ws))
+         | (key, ws) <- myExtraWorkspaces
+       ]
 
 --My Workspace Labels
+--wsp1 = ""
+--wsp2 = ""
+--wsp3 = ""
+--wsp4 = ""
+--wsp5 = ""
+--wsp6 = ""
+--wsp7 = ""
+--wsp8 = "8"
+--wsp9 = "9"
+--wsp0 = "monitor"
+
 wsp1 = "term"
+
 wsp2 = "web"
+
 wsp3 = "steam"
+
 wsp4 = "code"
+
 wsp5 = "media"
+
 wsp6 = "comms"
+
 wsp7 = "git"
+
 wsp8 = "8"
+
 wsp9 = "9"
+
 wsp0 = "monitor"
 
 myExtraWorkspaces = [(xK_0, wsp0)]
 
-myWorkspaces = [wsp1,wsp2,wsp3,wsp4,wsp5,wsp6,wsp7,wsp8,wsp9] ++ (map snd myExtraWorkspaces)
+myWorkspaces = [wsp1, wsp2, wsp3, wsp4, wsp5, wsp6, wsp7, wsp8, wsp9] ++ (map snd myExtraWorkspaces)
+
+-- My ScratchPads
+myScratchPads = [NS "terminal" spawnTerm findTerm manageTerm]
+  where
+    spawnTerm = myTerminal ++ " -T scratchpad"
+    findTerm = title =? "scratchpad"
+    manageTerm = customFloating $ W.RationalRect l t w h
+      where
+        h = 0.4
+        w = 0.5
+        t = (1 - h) / 4
+        l = (1 - w) / 2
 
 --
 --Custom Layouts
 --
 myLayout = avoidStruts (standardLayouts) LC.||| fullscreen
-    where
+  where
+    -- Percent of screen to increment by when resizing panes
+    delta = 3 / 100
 
-        -- Percent of screen to increment by when resizing panes
-      delta = 3/100
+    -- The default number of windows in the master pane
+    nmaster = 1
 
-      -- The default number of windows in the master pane
-      nmaster = 1
+    -- Default proportion of screen occupied by master pane
+    ratio = 1 / 2
 
-      -- Default proportion of screen occupied by master pane
-      ratio = 1/2
+    -- Tabbed layout configuration
+    myTabConfig =
+      def
+        { fontName = "xft:DDroid Sans Regular:size=10"
+        }
 
-      -- Tabbed layout configuration
-      myTabConfig = def {
-                   fontName = "xft:DDroid Sans Regular:size=10"
-                    }
+    -- Spacing/gaps
+    mySpacing = spacingRaw True (Border 0 10 10 10) True (Border 10 10 10 10) True
 
-      -- Spacing/gaps 
-      mySpacing = spacingRaw True (Border 0 10 10 10) True (Border 10 10 10 10) True 
+    -- Layouts
+    standardLayouts = smartBorders $ (tiled LC.||| Mirror tiled LC.||| myTab)
 
-      -- Layouts
-      standardLayouts = smartBorders $ ( tiled LC.||| Mirror tiled LC.||| myTab )
-
-      fullscreen = noBorders (fullscreenFull Full)
-      -- default tiling algorithm partitions the screen into two panes
-      tiled = mySpacing $ (Tall nmaster delta ratio)
-      -- tabbed algorithm with tabs at the top
-      myTab = tabbed shrinkText myTabConfig
+    fullscreen = noBorders (fullscreenFull Full)
+    -- default tiling algorithm partitions the screen into two panes
+    tiled = mySpacing $ (Tall nmaster delta ratio)
+    -- tabbed algorithm with tabs at the top
+    myTab = tabbed shrinkText myTabConfig
 
 --
---Custom Hooks 
+--Custom Hooks
 --
 
 -- Log Hooks
 -- Make window and workspace info available to xmobar
-myLogHook h = dynamicLogWithPP $ xmobarPP {
-            ppOutput = hPutStrLn h
-          , ppTitle = xmobarColor xmobarTitleColor "" .shorten 100
-          , ppCurrent = xmobarColor xmobarCurrentWorkspaceColor ""
-          , ppSep = "   "
-          }
+myLogHook h =
+  dynamicLogWithPP $
+    xmobarPP
+      { ppOutput = hPutStrLn h,
+        ppTitle = xmobarColor xmobarTitleColor "" . shorten 100,
+        ppCurrent = xmobarColor xmobarCurrentWorkspaceColor "",
+        ppSep = "   "
+      }
 
--- Manage Hooks 
-myManageHook = composeAll . concat $ 
-        [ 
-          -- Assign Applications to Workspaces
-          [className =? "firefox" --> doShift wsp2 ]
-        , [className =? "Steam" --> doShift wsp3 ]
-        , [className =? "Sublime_text" --> doShift wsp4 ]
-        , [className =? "code-oss" --> doShift wsp4 ]
-        , [className =? "Lollypop" --> doShift wsp5 ]
-        , [className =? "Signal" --> doShift wsp6 ]
-        , [className =? "discord" --> doShift wsp6 ]
-        , [className =? "Sublime_merge" --> doShift wsp7 ]
-        , [className =? "Xfce4-terminal" <&&> title =? "htop" --> doShift wsp0]
-        , [className =? "Xfce4-terminal" <&&> title =? "s-tui" --> doShift wsp0]
-        , [className =? "Xfce4-terminal" <&&> title =? "neofetch" --> doShift wsp0]
-          -- Assign windows to Float
-        , [className =? "Steam" <&&> title =? "Friends List" --> doFloat]
-        , [className =? "firefox" <&&> title =? "Close tabs?" --> doFloat]
+-- Manage Hooks
+myManageHook =
+  composeAll . concat $
+    [ -- Assign Applications to Workspaces
+      [className =? "firefox" --> doShift wsp2],
+      [className =? "Steam" --> doShift wsp3],
+      [className =? "Sublime_text" --> doShift wsp4],
+      [className =? "code-oss" --> doShift wsp4],
+      [className =? "Lollypop" --> doShift wsp5],
+      [className =? "Signal" --> doShift wsp6],
+      [className =? "discord" --> doShift wsp6],
+      [className =? "Sublime_merge" --> doShift wsp7],
+      [className =? "Xfce4-terminal" <&&> title =? "htop" --> doShift wsp0],
+      [className =? "Xfce4-terminal" <&&> title =? "s-tui" --> doShift wsp0],
+      [className =? "Xfce4-terminal" <&&> title =? "neofetch" --> doShift wsp0],
+      -- Assign windows to Float
+      [className =? "Steam" <&&> title =? "Friends List" --> doFloat],
+      [className =? "firefox" <&&> title =? "Close tabs?" --> doFloat]
+    ] 
+
+-- Game Hooks 
+myGameHook = 
+        composeAll . concat $
+        [
+          [className =? "CivBE" --> doShift wsp9 <+> doFullFloat ],
+          [stringProperty "STEAM_GAME(CARDINAL)" =? "65980" --> doShift wsp9 <+> doFullFloat],
+          [className =? "XCOM: Enemy Unknown" --> doShift wsp9 ],
+          [className =? "factorio" --> doShift wsp9 <+> doFullFloat]
         ]
+
 -- Startup Hooks
 myStartupHook = do
-  spawn "exe=`xcompmgr -c`"
+  --spawn "exe=`xcompmgr -c`"
+  spawn "exe=`picom -cC`"
   spawn "exe=`/home/jeffrey/.screenlayout/TwoWorkMonitors.sh`"
   spawnOnce (myTerminal ++ " -T htop -e htop")
-  spawnOnce (myTerminal ++ " -T s-tui -e s-tui") 
-  spawnOnce (myTerminal ++ " -T neofetch -e neofetch --hold") 
+  spawnOnce (myTerminal ++ " -T s-tui -e s-tui")
+  spawnOnce (myTerminal ++ " -T neofetch -e neofetch --hold")
   docksStartupHook <+> startupHook defaultConfig
-  
 
 --
 -- Custom Helper Functions
 --
 spawnToWorkspace :: String -> String -> X ()
-spawnToWorkspace program workspace = do 
-        spawn program
-        windows $ W.view workspace
-
-
+spawnToWorkspace program workspace = do
+  spawn program
+  windows $ W.view workspace
 
 -- | Select specified layout for current workspace
-chooseLayout ::  String -> X ()
+chooseLayout :: String -> X ()
 chooseLayout name = sendMessage $ JumpToLayout name
